34f1fb716134f28cdaaed314fdc29520503e5a28
Bisaso Ronald <bronnie313@gmail.com>
2023-05-31 20:39:21 +0300

    add more functional components

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,41 +1,38 @@
 const Cal = () => (
   <section>
-    <table>
-      <tr>
-        <th className="result" colSpan="4">0</th>
-      </tr>
-      <tr>
-        <td>AC</td>
-        <td>+/-</td>
-        <td>%</td>
-        <td className="last">÷</td>
-
-      </tr>
-      <tr>
-        <td>7</td>
-        <td>8</td>
-        <td>9</td>
-        <td className="last">x</td>
-      </tr>
-      <tr>
-        <td>4</td>
-        <td>5</td>
-        <td>6</td>
-        <td className="last">-</td>
-      </tr>
-      <tr>
-        <td>1</td>
-        <td>2</td>
-        <td>3</td>
-        <td className="last">+</td>
-      </tr>
-      <tr>
-        <td className="zero" colSpan={2}>0</td>
-        <td>.</td>
-        <td className="last">=</td>
-      </tr>
-    </table>
+    <CalculatorScreen />
+    <CalcBtns />
   </section>
+);
+
+const CalculatorScreen = () => (
+  <div className="screen">
+    <h2>0</h2>
+  </div>
+);
+
+const CalcBtns = () => (
+  <article className="keys">
+    <input type="button" value="AC" />
+    <input type="button" value="+/-" />
+    <input type="button" value="%" />
+    <input className="lastColumn" type="button" value="÷" />
+    <input type="button" value="7" />
+    <input type="button" value="8" />
+    <input type="button" value="9" />
+    <input className="lastColumn" type="button" value="x" />
+    <input type="button" value="4" />
+    <input type="button" value="5" />
+    <input type="button" value="6" />
+    <input className="lastColumn" type="button" value="-" />
+    <input type="button" value="1" />
+    <input type="button" value="2" />
+    <input type="button" value="3" />
+    <input className="lastColumn" type="button" value="+" />
+    <input className="zero" type="button" value="0" />
+    <input type="button" value="." />
+    <input className="lastColumn" type="button" value="=" />
+  </article>
 );
 
 export default Cal;
