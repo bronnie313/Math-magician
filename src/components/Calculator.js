121c2157a5aa78700feaaff0a58b10e660bfd8d8
Bisaso Ronald <bronnie313@gmail.com>
2023-05-31 17:30:07 +0300

    add calculator structure to the project

---
+++ src/components/Calculator.js
@@ -0,0 +1,41 @@
+const Cal = () => (
+  <section>
+    <table>
+      <tr>
+        <th className="result" colSpan="4">0</th>
+      </tr>
+      <tr>
+        <td>AC</td>
+        <td>+/-</td>
+        <td>%</td>
+        <td>÷</td>
+
+      </tr>
+      <tr>
+        <td>7</td>
+        <td>8</td>
+        <td>9</td>
+        <td>x</td>
+      </tr>
+      <tr>
+        <td>4</td>
+        <td>5</td>
+        <td>6</td>
+        <td>-</td>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+        <td>+</td>
+      </tr>
+      <tr>
+        <td className="zero" colSpan="2">0</td>
+        <td>.</td>
+        <td>=</td>
+      </tr>
+    </table>
+  </section>
+);
+
+export default Cal;
