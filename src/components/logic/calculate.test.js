c1ba1977d7f576028cd8b4e2a3698d170eb09a56
Bisaso Ronald <bronnie313@gmail.com>
2023-06-08 19:37:59 +0300

    add more tests to the calcualte test file

--- src/components/logic/calculate.test.js
+++ src/components/logic/calculate.test.js
@@ -12,4 +12,18 @@ describe('calculate', () => {
   it('should add a decimal point to the "next" value', () => {
     expect(calculate({ total: '4', next: '6', operation: null }, '.')).toEqual({ total: '4', next: '6.', operation: null });
   });
+
+  it('should calculate the result when "=" button is pressed', () => {
+    expect(calculate({ total: '5', next: '2', operation: '+' }, '=')).toEqual({ total: '7', next: null, operation: null });
+  });
+
+  it('should negate the "next" value when the plus/minus button is pressed', () => {
+    const result = calculate({ total: null, next: '2', operation: null }, '+/-');
+    expect(result).toEqual({ total: null, next: '-2', operation: null });
+  });
+
+  it('should update the operation when an operation button is pressed', () => {
+    const result = calculate({ total: '5', next: '2', operation: '+' }, '-');
+    expect(result).toEqual({ total: '7', next: null, operation: '-' });
+  });
 });
