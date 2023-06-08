38dc53507f1b1e288e0951ef11ec4a0c86c81496
Bisaso Ronald <bronnie313@gmail.com>
2023-06-08 13:13:38 +0300

    add calculate tests

---
+++ src/components/logic/calculate.test.js
@@ -0,0 +1,16 @@
+import calculate from './calculate';
+// import operate from "./operate";
+
+describe('calculate', () => {
+  it('should returns total null for object when AC is pressed', () => {
+    expect(calculate('{}', 'AC')).toEqual({ total: null, next: null, operation: null });
+  });
+
+  it('should update the "next" value when the buttonName is a number', () => {
+    expect(calculate({ total: '5', next: '2', operation: '+' }, '9')).toEqual({ total: '5', next: '29', operation: '+' });
+  });
+
+  it('should add a decimal point to the "next" value', () => {
+    expect(calculate({ total: '4', next: '6', operation: null }, '.')).toEqual({ total: '4', next: '6.', operation: null });
+  });
+});
